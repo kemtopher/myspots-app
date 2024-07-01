505702202db860b5f2d48971494868a521885732
Chris Kemeza <ckemeza1@gmail.com>
2024-06-30 20:01:42 -0400

    list scroll styles

--- src/components/ride-list/ride-list.js
+++ src/components/ride-list/ride-list.js
@@ -22,37 +22,43 @@ export const RideList = ({nodes}) => {
     },
     list: {
       height: '100%',
-      overflow: 'scroll'
     },
+    scrolllist: {
+      height: 'calc(100% - 60px)',
+      overflow: 'scroll'
+    }
   }
 
   return (
-    <section id="ride-list" style={ classes.list }>
-      { nodes.length ? (
-        <>
-          <Grid container sx={classes.header} display='flex'>
-            <Grid item xs={5}>
-              <Typography variant="h3" align={'left'} sx={classes.headerTitle}>Ride</Typography>
-            </Grid>
-            <Grid item xs={4}>
-              <Typography variant="h3" align={'left'} sx={classes.headerTitle}>Time</Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant="h3" align={'left'} sx={classes.headerTitle}>Actions</Typography>
-            </Grid>
+    <>
+      <section>
+        <Grid container sx={classes.header} display='flex'>
+          <Grid item xs={5}>
+            <Typography variant="h3" align={'left'} sx={classes.headerTitle}>Ride</Typography>
           </Grid>
-
-          { nodes.map(node => (
-            <RideCard key={node.id} data={node} />
-          )) }
-        </>
-      ) : (
-        <Box display="flex" alignItems="center" justifyContent="center" height="100%">
-          <Typography component="h1">
-            No rides to show yet ...
-          </Typography>
-        </Box>
-      ) }
-    </section>
+          <Grid item xs={4}>
+            <Typography variant="h3" align={'left'} sx={classes.headerTitle}>Time</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography variant="h3" align={'left'} sx={classes.headerTitle}>Actions</Typography>
+          </Grid>
+        </Grid>
+      </section>
+      <section id="ride-list" style={ classes.scrolllist }>
+        { nodes.length ? (
+          <>
+            { nodes.map(node => (
+              <RideCard key={node.id} data={node} />
+            )) }
+          </>
+        ) : (
+          <Box display="flex" alignItems="center" justifyContent="center" height="100%">
+            <Typography component="h1">
+              No rides to show yet ...
+            </Typography>
+          </Box>
+        ) }
+      </section>
+    </>
   );
 };
