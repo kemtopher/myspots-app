0e09914c05e495f6b827cbef9913e82d4ea5f003
Chris Kemeza <ckemeza1@gmail.com>
2025-10-06 21:34:28 -0400

    refactor: map performance was struggling from creating multiple instances of map, geolocations, and nav

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -10,6 +10,9 @@ mapboxgl.accessToken =
 export const Map = ({ events }) => {
   const mapContainer = useRef(null);
   const mapRef = useRef(null);
+  const geolocateRef = useRef(null);
+  const resizeObsRef = useRef(null);
+  
   const mapMarker = useRef(null);
   const dispatch = useDispatch();
   const tempCoords = useSelector((state) => state.coordinates.current);
@@ -19,56 +22,56 @@ export const Map = ({ events }) => {
 
   useEffect(() => {
     // map setup and maintenance
-    if (mapRef.current) {
-      if (mapMarker.current) {
-        mapMarker.current.remove();
-      }
+    if (!mapContainer.current) return;
 
-      mapRef.current.flyTo({
-        center: [tempCoords[0], tempCoords[1]],
-        essential: true
-      });
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/light-v11',
+      center: tempCoords,
+      zoom: 16
+    });
+    mapRef.current = map;
+
+    const nav = new mapboxgl.NavigationControl({
+      showZoom: true
+    });
+    map.addControl(nav, 'top-left');
+
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    });
+    map.addControl(geolocate, 'top-left');
+    geolocateRef.current = geolocate;
+
+    const onLoad = () => {
+      map.resize();
+      geolocate.trigger();
     }
+    
+    map.once('load', onLoad);
 
-    if (!mapRef.current) {
-      mapRef.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/light-v11',
-        center: [tempCoords[0], tempCoords[1]],
-        zoom: 16
-      });
+    const onGeolocate = (e) => {
+      setGeoCoords([e.coords.longitude, e.coords.latitude]);
+    };
+    geolocate.on('geolocate', onGeolocate);
 
-      const nav = new mapboxgl.NavigationControl({
-        showZoom: true
-      });
+    const resizer = new ResizeObserver(() => map.resize());
+    resizeObsRef.current = resizer;
+    resizer.observe(mapContainer.current);
 
-      const geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        trackUserLocation: true,
-        showUserHeading: true
-      });
-
-      mapRef.current.addControl(nav, 'top-left');
-      mapRef.current.addControl(geolocate, 'top-left');
-      // add full screen control
-      // mapRef.current.addControl(new mapboxgl.FullscreenControl(), 'top-left');
-
-      mapRef.current.on('load', () => {
-        mapRef.current.resize();
-        geolocate.trigger();
-      });
-
-      geolocate.on('geolocate', function (e) {
-        const lon = e.coords.longitude;
-        const lat = e.coords.latitude;
-        setGeoCoords([lon, lat]);
-      });
+    return () => {
+      geolocate.off('geolocate', onGeolocate);
+      map.off('load', onLoad);
+      resizer.disconnect();
+      map.remove();
+      mapRef.current = null;
+      geolocateRef.current = null;
     }
-
-    return () => {}
-  }, [tempCoords]);
+  }, []);
 
   useEffect(() => {
     // marker setup
